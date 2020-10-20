dbc4d16ac1dc06c4a93c24fb68c7bf6a16d05988
educr0w <g1tCr0#1235>
2020-10-20 01:23:21 -0300

    First commit

---
+++ src/test/java/ar/net/grid/joke/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.net.grid.joke.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
